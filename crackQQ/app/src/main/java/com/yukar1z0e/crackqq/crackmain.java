8a448bcf4d404aa09e209452241d8ed92f24eb68
yukar1z0e <44317939+yukar1z0e@users.noreply.github.com>
2019-09-03 16:58:28 +0800

    I AM IN
    Signed-off-by: yukar1z0e <44317939+yukar1z0e@users.noreply.github.com>

---
+++ crackQQ/app/src/main/java/com/yukar1z0e/crackqq/crackmain.java
@@ -0,0 +1,50 @@
+package com.yukar1z0e.crackqq;
+
+import android.app.Application;
+import android.content.Context;
+import android.os.FileObserver;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.view.MotionEvent;
+
+import java.text.Format;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.LinkedList;
+import java.util.Objects;
+import java.util.concurrent.RunnableFuture;
+import java.util.concurrent.TimeUnit;
+
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+import static de.robv.android.xposed.XposedHelpers.assetAsByteArray;
+import static de.robv.android.xposed.XposedHelpers.findAndHookConstructor;
+import static de.robv.android.xposed.XposedHelpers.findClass;
+import static de.robv.android.xposed.XposedHelpers.findField;
+import static de.robv.android.xposed.XposedHelpers.callMethod;
+import static de.robv.android.xposed.XposedHelpers.setObjectField;
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+
+public class crackmain implements IXposedHookLoadPackage {
+    private XC_LoadPackage.LoadPackageParam lpparam = null;
+
+    @Override
+    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
+        if (loadPackageParam.packageName.contains("com.tencent")) {
+            findAndHookMethod(Application.class, "attach", Context.class, new XC_MethodHook() {
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    lpparam = loadPackageParam;
+                    Log.d("crackQQ","I am in "+lpparam.packageName);
+                }
+            });
+        }
+    }
+}
